19bdd8de71d8cb7e3296c4b620655bf28be23843
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-20 21:52:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 function threeSum(arr, target) {
-// write your code here
-  while(target>0){
-  arr.push(target%2);
-  target= parseInt(target/2);
-}
-arr.reverse();
+
 }
 
 module.exports = threeSum;
